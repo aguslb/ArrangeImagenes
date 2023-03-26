ff42e5907deb10e09cf7bd46b36aa00aca054c2d
Jose Agustin Velasco Torres <agusmac@Agus-MacBook-Pro.local>
2023-03-26 01:25:59 -0400

    Fix fro mac oS

--- src/main/java/org/arrangeImagenes/ReadProperties.java
+++ src/main/java/org/arrangeImagenes/ReadProperties.java
@@ -4,15 +4,20 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.Set;
 
 public class ReadProperties {
 
     private static final String WIN_APP_PROP = "C:\\Users\\zid_0\\IdeaProjects\\ArrangeImagenes\\src\\main\\resources\\application-win.properties";
     private static final String UNIX_APP_PROP = "/Users/agusmac/Proyectos/ArrangeImagenes/src/main/resources/application-unix.properties";
 
+    private static final String MACOSX_APP_PROP = "/Users/agusmac/Proyectos/ArrangeImagenes/src/main/resources/application-osx.properties";
+
     public static Properties readPropertiesFile(String os) {
         if (os.contains("Windows"))
             return readPropertiesFromProjectFile(WIN_APP_PROP);
+        if (os.contains("OSX"))
+            return readPropertiesFromProjectFile((MACOSX_APP_PROP));
         return readPropertiesFromProjectFile(UNIX_APP_PROP);
     }
 
@@ -21,7 +26,10 @@ public class ReadProperties {
         try (InputStream input = new FileInputStream(path)) {
             // load a properties file
             prop.load(input);
-            System.setProperties(prop);
+            for (Object key:
+                 prop.keySet()) {
+                System.setProperty(key.toString(),prop.getProperty(key.toString()));
+            }
             return prop;
         } catch (IOException ex) {
             ex.printStackTrace();
